6fb79ce670a597970c8af52da355fbb41f3946f0
WMAlves <williamma03@gmail.com>
2022-11-12 19:47:15 -0300

    criação do arquivos index.html, calculadora.css, calculadora.js, formatação do css, e inicio dos tratamentos dos botões.

---
+++ calculadora.js
@@ -0,0 +1,43 @@
+onload=()=>{
+    document.querySelector('#bt-0').onclick = ()=> digito(0);
+    document.querySelector('#bt-1').onclick = ()=> digito(1);
+    document.querySelector('#bt-2').onclick = ()=> digito(2);
+    document.querySelector('#bt-3').onclick = ()=> digito(3);
+    document.querySelector('#bt-4').onclick = ()=> digito(4);
+    document.querySelector('#bt-5').onclick = ()=> digito(5);
+    document.querySelector('#bt-6').onclick = ()=> digito(6);
+    document.querySelector('#bt-7').onclick = ()=> digito(7);
+    document.querySelector('#bt-8').onclick = ()=> digito(8);
+    document.querySelector('#bt-9').onclick = ()=> digito(9);
+    document.querySelector('#bt-comma').onclick = ()=> virgula();
+
+}
+
+// variaveis para armazenamento de valor, o operador e o estado da calculadora
+
+let sValor='0'
+let novoNumero=true;
+
+const atualizaVisor=()=>{
+    document.getElementById('display').innerHTML=sValor;
+}
+
+//função do tratamento de botões de numeros
+const digito =(n)=>{
+    if(novoNumero){
+        sValor=''+ n;
+        novoNumero=false;
+    }else
+    sValor+=n;
+    atualizaVisor();      
+}
+
+//tratamento de clic no botão de ponto decimal
+const virgula=()=>{
+    if(novoNumero){
+        sValor='0, ';
+        novoNumero=false;
+    }else if(sValor.indexOf(',')==-1)
+        sValor+=','
+    atualizaVisor
+}
